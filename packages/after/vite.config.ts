96ed04c2f770db59159be52280707e98def8e339
sori kim <65494214+milmilkim@users.noreply.github.com>
2025-11-24 23:42:11 +0900

    배포

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -3,8 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+const base: string =
+  process.env.NODE_ENV === "production" ? "/front_7th_chapter3-1/" : "";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
