2181ea21bf072f7150e7563eeac9bff283e8d056
sori kim <soundkim27@gmail.com>
2025-11-24 13:59:05 +0900

    카드 컴포넌트

---
+++ packages/after/src/stories/card.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+const meta = {
+  title: "UI/Card",
+  component: Card,
+  parameters: {},
+  tags: ["autodocs"],
+} satisfies Meta<typeof Card>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <CardAction>
+          <Button>Card Action</Button>
+        </CardAction>
+      </CardFooter>
+    </Card>
+  ),
+};
